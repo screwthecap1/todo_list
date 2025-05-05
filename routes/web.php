28bcfcfa8156850e568b899f882bdd99b0f4c1be
screwthecap1 <chanskiy777@mail.ru>
2025-05-05 20:49:03 +0400

    First commit Day 1: - Creating Laravel Project include BS - SQLite bd connection - First migration and model setting (Task) - Route structure as Task CRUD

---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Task\IndexController as TaskIndexController;
+use App\Http\Controllers\Task\StoreController as TaskStoreController;
+use App\Http\Controllers\Task\UpdateController as TaskUpdateController;
+use App\Http\Controllers\Task\DestroyController as TaskDestroyController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::middleware('auth')->prefix('tasks')->group(function () {
+    Route::get('/', TaskIndexController::class)->name('task.index');
+    Route::post('/', TaskStoreController::class)->name('task.store');
+    Route::patch('/{task}', TaskUpdateController::class)->name('task.update');
+    Route::delete('/{task}' . TaskDestroyController::class)->name('task.destroy');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
